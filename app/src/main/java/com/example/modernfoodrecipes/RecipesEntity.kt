3c6a79b08b553c0102b1ade985c183e5b8c7924a
javad <j.shirkhani@clickhub.ir>
2022-03-23 00:21:34 +0430

    RecipesEntity added !

---
+++ app/src/main/java/com/example/modernfoodrecipes/RecipesEntity.kt
@@ -0,0 +1,16 @@
+package com.example.modernfoodrecipes
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.modernfoodrecipes.models.FoodRecipe
+import com.example.modernfoodrecipes.util.Constants
+
+
+@Entity(tableName = Constants.RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
--- app/src/main/java/com/example/modernfoodrecipes/util/Constants.kt
+++ app/src/main/java/com/example/modernfoodrecipes/util/Constants.kt
@@ -5,6 +5,11 @@ class Constants {
     companion object {
         const val BASE_URL = "https://api.spoonacular.com"
         const val API_KEY = "61f44c8f52c548b98eac65d5cfdb8646"
+
+
+        // Room Constant
+        const val DATABASE_NAME = "recipes_database"
+        const val RECIPES_TABLE = "recipes_table"
     }
 
 }
