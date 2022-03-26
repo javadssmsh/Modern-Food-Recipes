36780cf70d836957635c90e140a3850407dbf7f1
javad <j.shirkhani@clickhub.ir>
2022-03-27 01:54:16 +0430

    Create RecipesDatabase.kt

---
+++ app/src/main/java/com/example/modernfoodrecipes/RecipesDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.modernfoodrecipes
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [RecipesEntity::class], version = 1, exportSchema = false)
+abstract class RecipesDatabase : RoomDatabase() {
+
+    abstract fun recipesDao():RecipesDao
+
+}
