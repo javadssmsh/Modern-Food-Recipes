f7d4dceb84185224afe987121779a46b31e25f6c
javad <j.shirkhani@clickhub.ir>
2022-03-27 23:01:41 +0430

    local data source added

---
+++ app/src/main/java/com/example/modernfoodrecipes/data/LocalDataSource.kt
@@ -0,0 +1,19 @@
+package com.example.modernfoodrecipes.data
+
+import com.example.modernfoodrecipes.data.database.RecipesDao
+import com.example.modernfoodrecipes.data.database.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+    private val recipesDao: RecipesDao
+) {
+
+    fun readDatabase(): Flow<List<RecipesEntity>> {
+        return recipesDao.readRecipes()
+    }
+
+    suspend fun insertRecipes(recipesEntity: RecipesEntity) {
+        recipesDao.insertRecipes(recipesEntity)
+    }
+}
