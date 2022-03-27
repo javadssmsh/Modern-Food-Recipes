68c939a5baf302d48a2403cf4c4c247ddf135123
javad <j.shirkhani@clickhub.ir>
2022-03-27 22:50:58 +0430

    Create DatabaseModule.kt

---
+++ app/src/main/java/com/example/modernfoodrecipes/di/DatabaseModule.kt
@@ -0,0 +1,35 @@
+package com.example.modernfoodrecipes.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.modernfoodrecipes.data.database.RecipesDatabase
+import com.example.modernfoodrecipes.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext
+        context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        Constants.DATABASE_NAME
+    ).build()
+
+
+    @Singleton
+    @Provides
+    fun provideDao(database: RecipesDatabase) = database.recipesDao()
+
+
+}
