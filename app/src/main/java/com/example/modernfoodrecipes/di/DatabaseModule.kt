f73ccdc1deacd1ef27b888648961e2e891b9c967
javad <j.shirkhani@clickhub.ir>
2022-03-29 17:41:17 +0430

    bug fix (annotation)

--- app/src/main/java/com/example/modernfoodrecipes/di/DatabaseModule.kt
+++ app/src/main/java/com/example/modernfoodrecipes/di/DatabaseModule.kt
@@ -8,11 +8,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 
 @Module
-@InstallIn
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
     @Singleton
