a21b61f13fe237c898db3583b4d6faf8fc0282ca
javad <j.shirkhani@clickhub.ir>
2022-03-29 17:40:55 +0430

    bug fix (converter annotation )

--- app/src/main/java/com/example/modernfoodrecipes/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/example/modernfoodrecipes/data/database/RecipesDatabase.kt
@@ -2,8 +2,10 @@ package com.example.modernfoodrecipes.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [RecipesEntity::class], version = 1, exportSchema = false)
+@TypeConverters(RecipeTypeConverter::class)
 abstract class RecipesDatabase : RoomDatabase() {
 
     abstract fun recipesDao(): RecipesDao
