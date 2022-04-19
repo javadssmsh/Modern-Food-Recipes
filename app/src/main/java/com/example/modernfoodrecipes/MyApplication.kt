ad7650e3317c632380775412079de99b96b64491
javadssmsh <javadssmh@gmail.com>
2022-04-19 16:28:24 +0430

    connectivity aware added  !

--- app/src/main/java/com/example/modernfoodrecipes/MyApplication.kt
+++ app/src/main/java/com/example/modernfoodrecipes/MyApplication.kt
@@ -1,9 +1,21 @@
 package com.example.modernfoodrecipes
 
 import android.app.Application
+import android.util.Log
+import androidx.lifecycle.Observer
+import com.example.modernfoodrecipes.util.ConnectionLiveData
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val connectionLiveData = ConnectionLiveData(applicationContext)
+        connectionLiveData.observeForever {
+            Log.d("observeForever", "connectionLiveData state  :  $it")
+        }
+    }
 }
